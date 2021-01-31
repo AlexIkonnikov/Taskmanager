a697496b52ed64b5e733d22e9b8a8ad6c31b120e
Alex <sashaikonnikov55@gmail.com>
2021-01-31 20:55:12 +0700

    Added model.(MVC)

---
+++ src/models/tasks.js
@@ -0,0 +1,35 @@
+
+export default class Tasks {
+  constructor() {
+    this._tasks = [];
+    this._dataChangeHandler = [];
+  }
+
+  getTasks() {
+    return this._tasks;
+  }
+
+  setTasks(tasks) {
+    this._tasks = Array.from(tasks);
+    this._callHandlers(this._dataChangeHandler);
+  }
+
+  updateTask(id, task) {
+    const index = this._tasks.findIndex((task) => task.id === id);
+
+    if (index === -1) {
+      return;
+    }
+
+    this._tasks = [].concat(this._tasks.slice(0, index), task, this._tasks.slice(index + 1));
+    return true;
+  }
+
+  setDataChangeHandler(handler) {
+    this._dataChangeHandler.push(handler);
+  }
+
+  _callHandlers(handlers) {
+    handlers.forEach((handler) => handler());
+  }
+}
